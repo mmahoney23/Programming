3f69bdd820691180ea823cfd1131b47849d0fd03
mmahoney23 <michael.mahoney@gopalions.org>
2016-01-28 20:12:44 -0600

    Update ProgramAwesome.java

--- src/ProgramAwesome.java
+++ src/ProgramAwesome.java
@@ -1,7 +1,7 @@
 import java.util.Objects;
 import java.util.Scanner;
 
-class Questions {
+class ProgramAwesome {
     public static void main(String[] args) {
         String name;
         int age;
